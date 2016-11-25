ba064ae33ee4f5ce0b1eb7a5a320231919cf59ec
Oscar Duran <xoscarduranx@gmail.com>
2016-11-25 17:29:30 +0100

    add task

--- resources/assets/js/components/TodosList.vue
+++ resources/assets/js/components/TodosList.vue
@@ -1,12 +1,44 @@
 <template>
 <div>
         <div class="box">
+            <div class="box box-info">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Add Task</h3>
+                </div>
+                <!-- /.box-header -->
+                <!-- form start -->
+                <form role="form" action="#">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="name" class="form-control" id="name" placeholder="Enter task name here"
+                                   v-model="newTodo"
+                                   @keyup.enter="addTodo">
+                        </div>
+                    </div>
+                </form>
+            </div>
             <div class="box-header with-border">
                 <h3 class="box-title">Tasques</h3>
+
+                <div class="btn-group">
+                    <button type="button" class="btn btn-info">{{visibility}}</button>
+                    <button type="button" class="btn btn-info btn-flat dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                        <span class="caret"></span>
+                        <span class="sr-only">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#" v-on:click="setVisibility('all')">All</a></li>
+                        <li><a href="#" v-on:click="setVisibility('active')">Active</a></li>
+                        <li><a href="#" @click="setVisibility('completed')">Done</a></li>
+                    </ul>
+                </div>
+
             </div>
             <!-- /.box-header -->
             <div class="box-body">
                 <table class="table table-bordered">
+
                     <thead>
                     <tr>
                         <th style="width: 10px">#</th>
@@ -18,7 +50,7 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <tr v-for="(todo, index) in todos">
+                    <tr v-for="(todo, index) in filteredTodos">
                         <td>{{index + 1}}</td>
                         <td>{{todo.name}}</td>
                         <td>{{todo.priority}}</td>
@@ -60,14 +92,54 @@
                 message: 'Hello Vue que tal!',
                 seen: false,
                 todos: [],
+                visibility: 'all',
+                newTodo: ''
             }
          },
+
+         computed: {
+            filteredTodos: function(){
+                var filters = {
+                    all: function(todos){
+                        return todos;
+                    },
+                    active: function(todos){
+                        return todos.filter(function(todo){
+                            return !todo.done;
+                        });
+                    },
+                    completed: function(todos){
+                         return todos.filter(function(todo){
+                            return todo.done;
+                         });
+                    },
+                };
+
+                return filters[this.visibility](this.todos);
+            }
+         },
+
          methods: {
+            addTodo: function() {
+                var value = this.newTodo && this.newTodo.trim();
+                if (!value) {
+                    return;
+                }
+                this.todos.push({
+                    name: value,
+                    priority: 1,
+                    done: false
+                });
+                this.newTodo = '';
+            },
+            setVisibility: function(visibility) {
+                console.log(visibility);
+                this.visibility = visibility;
+            },
             reverseMessage:function () {
                 this.message= this.message.split('').reverse().join('');
             },
             fetchData: function() {
-                console.log('hola');
             // GET /someUrl
                 this.$http.get('/api/v1/task').then((response) => {
                     this.todos = response.data.data;
@@ -78,11 +150,13 @@
                     console.log(response);
                 });
             },
+
+
+
          },
          created: function() {
             console.log('Component created!');
-            this.fetchData()
-
+            this.fetchData();
          }
 
 
