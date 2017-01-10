60c61011a2a475f40d22daa34e0a8c36e397bc55
Oscar Duran <xoscarduranx@gmail.com>
2017-01-10 18:11:29 +0100

    sync

--- resources/views/boxmodel.blade.php
+++ resources/views/boxmodel.blade.php
@@ -21,7 +21,7 @@
 		width: 600px;
 		margin: 20px auto;
 		border: solid green 10px;
-
+		box-sizing: border-box;
 	}
 
 	#bloc2
@@ -32,7 +32,20 @@
 		margin: 20px auto;
 		padding: 50px;
 		border: solid blue 10px;
-		border-width: 10px;
+		box-sizing: border-box;
+	}
+
+	#bloc3
+	{
+		position: absolute;
+		top: 0px;
+		right:0px;
+		margin: 20px auto;
+		padding: 50px;
+		border: solid blue 10px;
+		box-sizing: border-box;
+		width: 600px;
+
 	}
 
 </style>
@@ -47,7 +60,17 @@
 
 <div id="bloc2">
 	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi architecto beatae, dicta ducimus error, explicabo incidunt ipsam ipsum iusto laudantium libero magni maxime,
+
+
+	<div id="bloc3">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, aut dignissimos distinctio dolore earum est inventore, maxime minus nam nostrum perspiciatis quaerat reprehenderit repudiandae totam veniam! Dolorum itaque laboriosam perspiciatis.
+	</div>
+
 </div>
+
+
+
+
 
 </body>
 </html>
