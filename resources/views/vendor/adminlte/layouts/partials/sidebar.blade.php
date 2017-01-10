9bba58b16343b9aac6cd0aa621a49e3e124982f1
Oscar Duran <xoscarduranx@gmail.com>
2017-01-10 17:16:56 +0100

    sync

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -30,14 +30,8 @@
         <!-- /.search form -->
 
         <!-- Sidebar Menu -->
-        <ul class="sidebar-menu">
-            <li class="header">{{ trans('adminlte_lang::message.header') }}</li>
-            <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
-            <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
-            <li><a href="{{ url('tasks') }}"><i class='fa fa-link'></i> <span>Taskes</span></a></li>
-            <li><a href="{{ url('profile/tokens') }}"><i class='fa fa-link'></i> <span>Tokens</span></a></li>
-        </ul><!-- /.sidebar-menu -->
+        {{ Menu::sidebar() }}
+        <!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
 </aside>
