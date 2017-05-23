a09112e560ba1f994e916c36819d1a72f365d359
Oscar Duran <xoscarduranx@gmail.com>
2017-05-23 19:27:23 +0200

    Update routes api

--- routes/api.php
+++ routes/api.php
@@ -11,12 +11,12 @@
 //});
 
 //Route::group(['prefix' => 'v1','middleware' => 'auth:api'], function () {
-Route::group(['prefix' => 'v1','middleware' => 'cors'], function () {
+Route::group(['prefix' => 'v1','middleware' => ['cors', 'auth:api']], function () {
     Route::resource('task', 'TasksController');
     Route::resource('user', 'UsersController');
     Route::resource('user.task', 'UserTasksController');
-    Route::get('/user', function (Request $request){
-        return $request->user();
+    Route::get('/user', function (){
+        return Auth::user();
     });
 });
 
